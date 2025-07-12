df19b89fd57b735e56456c48813c1a8882bd1ff2
grifin <168100548+EnyaGalvez@users.noreply.github.com>
2025-07-12 17:05:13 -0600

    helloworld

---
+++ rust-helloworld/target/debug/deps/rust_helloworld.d
@@ -0,0 +1,5 @@
+C:\Users\galve\a2c2e\graficasporcompu\rust-helloworld\target\debug\deps\rust_helloworld.d: src\main.rs
+
+C:\Users\galve\a2c2e\graficasporcompu\rust-helloworld\target\debug\deps\rust_helloworld.exe: src\main.rs
+
+src\main.rs:
---
+++ rust-helloworld/target/debug/rust-helloworld.d
@@ -0,0 +1,1 @@
+C:\Users\galve\a2c2e\graficasporcompu\rust-helloworld\target\debug\rust-helloworld.exe: C:\Users\galve\a2c2e\graficasporcompu\rust-helloworld\src\main.rs
