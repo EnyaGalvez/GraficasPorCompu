42f32321d4b7c21d4516cc9075fdaee42f950b68
grifin <168100548+EnyaGalvez@users.noreply.github.com>
2025-07-13 21:36:33 -0600

    Update main.rs

--- raylib_prettyline/src/main.rs
+++ raylib_prettyline/src/main.rs
@@ -4,8 +4,8 @@ mod figures;
 
 use raylib::prelude::*;
 use crate::framebuffer::Framebuffer;
-use bhm_line::{bhm_line,LineaBonita};
-use figures::dibujar_triangulo_regular;
+use crate::bhm_line::{bhm_line, LineaBonita};
+use crate::figures::dibujar_triangulo_regular;
 
 fn main() {
     let image_width = 800;
@@ -15,14 +15,16 @@ fn main() {
     framebuffer.set_background_color(Color::BLACK);
     framebuffer.clear();
 
+    // Dibuja una línea
     framebuffer.set_current_color(Color::CYAN);
     bhm_line(
         &mut framebuffer,
         LineaBonita::new(60, 60),
-        LineaBonita::new(600, 100),
+        LineaBonita::new(600, 600),
         3, //Grosor de la línea
     );
 
+    // Dibuja un triángulo regular
     framebuffer.set_current_color(Color::MAGENTA);
     dibujar_triangulo_regular(
         &mut framebuffer,
@@ -31,7 +33,7 @@ fn main() {
         3, //Grosor de la línea
     );
 
-    let output_file_name ="mkline_image.png";
+    let output_file_name ="line_fig_image.png";
 
     framebuffer.render_to_file(output_file_name);
 }
